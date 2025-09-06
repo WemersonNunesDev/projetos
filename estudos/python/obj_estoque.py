8adf6b7afad267575197a08c9ac37d41c3b7c729
WemersonNunesDev <wemersononunes@gmail.com>
2025-09-06 16:03:29 -0300

    feat: updated somethings

--- estudos/python/obj_estoque.py
+++ estudos/python/obj_estoque.py
@@ -16,7 +16,7 @@ class Estoque:
         valor_venda = locale.currency(self.preco_venda, grouping=True)
         total_preco_venda = locale.currency(self.quantidade * self.preco_venda, grouping=True)
 
-        texto = f'{self.nome.capitalize()}\n{round(self.quantidade, 3)} {valor} - Total: {valor_total}  {valor_venda} - Total: {total_preco_venda}\n{'=' * 56}'
+        texto = f'{self.nome.capitalize()}\n{round(self.quantidade, 3)} {valor} - Total: {valor_total}  {valor_venda} - Total: {total_preco_venda}\n{'=' * 72}\n'
 
         with open('estoque.txt', 'a', encoding='utf-8') as f:
             f.write(texto)
@@ -24,13 +24,6 @@ class Estoque:
 
     @staticmethod
     def ler_estoque():
-        title = 'Item'
-        qtd = 'Qtd'
-        text_compra = 'Valor de compra'
-        text_total = 'Total'
-        text_venda = 'Valor de venda'
-
-        # print(title.center(24, ' '), qtd.center(24, ' '), text_compra.center(24, ' '), text_total.center(24, ' '), text_venda.center(24, ' '), text_total.center(24, ' '))
         try:
             with open('estoque.txt', 'r', encoding='utf-8') as f:
                 conteudo = f.read()
