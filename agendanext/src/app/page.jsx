9231254abcad25151f1e0057072353b15d14a22c
WemersonNunesDev <wemersononunes@gmail.com>
2025-11-19 14:02:31 -0300

    Teste de projeto

--- agendanext/src/app/page.jsx
+++ agendanext/src/app/page.jsx
@@ -1,7 +1,7 @@
 import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-import Textalert from "@/components/Textalert";
-import Contato from "@/components/Contato";
+// import Footer from "@/components/Footer";
+// import Textalert from "@/components/Textalert";
+// import Contato from "@/components/Contato";
 
 export default function Home() {
   return (
@@ -11,26 +11,26 @@ export default function Home() {
         <section className="text-center text-gray-950 mt-4 mb-8">
           <h1 className="text-5xl font-semibold">Agenda</h1>
           <p className="my-3">Seus contatos estão abaixo</p>
-          <Textalert 
+          {/* <Textalert 
             text={'Nenhum contato cadastrado ainda'}
             color={'rose'}
-          />
+          /> */}
         </section>
 
         <section>
           <table className="w-full table-auto">
             <tbody>
-              <Contato
+              {/* <Contato
                 nome={'wemerson'}
                 sobrenome={'nunes'}
                 celular={'369845698712'}
                 email={'nomealeatorio@email.com'}
-              />
+              /> */}
             </tbody>
           </table>
         </section>
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
